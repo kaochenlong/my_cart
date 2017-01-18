7ff29032e091bb09f5903047948bbda02057a651
Eddie Kao <eddie@digik.com.tw>
2017-01-18 19:56:52 +0800

    add cart test

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,22 +1,41 @@
 require 'rails_helper'
 
 class Cart
+  def initialize
+    @items = []
+  end
+
+  def empty?
+    @items.empty?
+  end
+
+  def add_item(product_id)
+    @items << product_id
+  end
 end
 
 RSpec.describe Cart, type: :model do
 
-#基本功能：
+describe "基本功能" do
+  it "可以新增商品到購物車裡，然後購物車裡就有東西了。" do
+    cart = Cart.new
+    expect(cart.empty?).to be true
 
-#1. 可以新增商品到購物車裡，然後購物車裡就有東西了。
-#2. 如果加了相同種類的商品到購物車裡，購買項目(CartItem)並不會增加，但數量會改變。
-#3. 商品可以放到購物車裡，也可以再拿出來。
-#4. 可以計算整台購物車的總消費金額。
+    cart.add_item(1)
+    expect(cart.empty?).to be false
+  end
 
-#進階功能：
+  # 如果加了相同種類的商品到購物車裡，購買項目(CartItem)並不會增加，但數量會改變。
+  # 商品可以放到購物車裡，也可以再拿出來。
+  # 可以計算整台購物車的總消費金額。
+  # 每個 Cart Item 都可以計算它自己的金額(小計)
+  # 特別活動可能可搭配折扣(例如聖誕節的時候全面打 9 折，或是滿額滿千送百)
+end
 
-#* 因為購物車將以 Session 方式儲存，所以：
-  #* 可以將購物車內容轉換成 Hash
-  #* 也可以把 Hash 還原成購物車的內容
+describe "進階功能" do
+#* 可以將購物車內容轉換成 Hash
+#* 也可以把 Hash 還原成購物車的內容
+end
 
 end
 
