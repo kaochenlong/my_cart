9e4bcc130327e8e704e39c4a842a1128decce959
Eddie Kao <eddie@digik.com.tw>
2017-01-22 11:49:34 +0800

    add order item

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170122033859) do
+ActiveRecord::Schema.define(version: 20170122034754) do
+
+  create_table "order_items", force: :cascade do |t|
+    t.integer  "product_id"
+    t.integer  "order_id"
+    t.integer  "quantity"
+    t.integer  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+    t.index ["product_id"], name: "index_order_items_on_product_id"
+  end
 
   create_table "orders", force: :cascade do |t|
     t.string   "receipent"
